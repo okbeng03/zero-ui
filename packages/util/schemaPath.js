df883e85dd6170a315858c4dceecb10bfb0b2e06
chb.wang <chb.wang@hnair.com>
2020-07-30 11:22:06 +0800

    路径转换成objectpath标准路径

--- packages/util/schemaPath.js
+++ packages/util/schemaPath.js
@@ -1,7 +1,8 @@
 import traverse from 'json-schema-traverse'
-import ObjectPath from 'objectpath'
 
-const regKeyword = /\/(items|properties)/g
+const regPropertiees = /\/properties/g
+const regItems = /\/items/g
+const regSplit = /\//g
 
 /**
  * 遍历 jsonSchema，得到 pathMap
@@ -14,8 +15,11 @@ function schemaPath (jsonSchema) {
 
   traverse(jsonSchema, function (schema, pointer, root, parentPointer, parentKeyword, parentSchema, property) {
     if (pointer) {
-      key = pointer.replace(regKeyword, '').substr(1).split('/')
-      pathMap[ObjectPath.stringify(key)] = schema
+      key = pointer.replace(regPropertiees, '').replace(regItems, '[0]').substr(1).replace(regSplit, '.')
+      pathMap[key] = {
+        _property: property || '',
+        ...schema
+      }
     }
   })
 
