1946696767c5eb176430f3745c48cec869421933
chb.wang <chb.wang@hnair.com>
2020-07-30 15:16:38 +0800

    所有类型demo

--- examples/src/pages/form/App.vue
+++ examples/src/pages/form/App.vue
@@ -17,317 +17,105 @@ export default {
         form: {
           items: [
             {
-              key: 'navs',
-              type: 'array',
-              items: [
-                {
-                  key: 'navs[]',
-                  type: 'fieldset',
-                  items: [
-                    'navs[].title',
-                    '*'
-                  ]
-                }
-              ]
+              key: 'name',
+              lazy: false
             },
-            '*',
             {
-              key: 'icp',
-              type: 'filedset',
-              items: [
-                '*'
-              ]
-            }
+              key: 'age',
+              type: 'a-slider',
+              decorator: {}
+            },
+            'single',
+            {
+              key: 'sex',
+              input: {
+                options: [
+                  {
+                    label: '男',
+                    value: '0'
+                  },
+                  {
+                    label: '女',
+                    value: '1'
+                  }
+                ]
+              }
+            },
+            'hobby',
+            {
+              key: 'other',
+              type: 'textarea'
+            },
+            {
+              key: 'birthday'
+            },
+            '*'
           ]
         }
       },
       schema: {
-        "$schema": "http://json-schema.org/draft-07/schema#",
-        "$id": "http://example.com/root.json",
-        "type": "object",
-        "title": "The Root Schema",
-        "required": [
-          "navs",
-          "friendLinks"
-        ],
-        "properties": {
-          "navs": {
-            "$id": "#/properties/navs",
-            "type": "array",
-            "title": "左侧导航",
-            "maxItems": 3,
-            "minItems": 3,
-            "items": {
-              "$id": "#/properties/navs/items",
-              "type": "object",
-              "title": "",
-              "required": [
-                "title",
-                "list"
-              ],
-              "properties": {
-                "title": {
-                  "$id": "#/properties/navs/items/properties/title",
-                  "type": "string",
-                  "title": "一级菜单"
-                },
-                "list": {
-                  "$id": "#/properties/navs/items/properties/list",
-                  "type": "array",
-                  "title": "二级菜单",
-                  "maxItems": 8,
-                  "items": {
-                    "$id": "#/properties/navs/items/properties/list/items",
-                    "type": "object",
-                    "title": "",
-                    "required": [
-                      "url",
-                      "target",
-                      "title"
-                    ],
-                    "properties": {
-                      "title": {
-                        "$id": "#/properties/navs/items/properties/list/items/properties/title",
-                        "type": "string",
-                        "title": "标题"
-                      },
-                      "url": {
-                        "$id": "#/properties/navs/items/properties/list/items/properties/url",
-                        "type": "string",
-                        "title": "链接"
-                      },
-                      "target": {
-                        "$id": "#/properties/navs/items/properties/list/items/properties/target",
-                        "type": "string",
-                        "title": "打开方式",
-                        "default": "_self",
-                        "enum": [
-                          "_self",
-                          "_blank"
-                        ]
-                      }
-                    }
-                  }
-                }
-              }
+        title: 'basic',
+        type: 'object',
+        properties: {
+          name: {
+            type: 'string',
+            title: '姓名'
+          },
+          age: {
+            type: 'number',
+            title: '年龄',
+            minimum: 1,
+            maximum: 120,
+            default: 25
+          },
+          single: {
+            type: 'boolean',
+            title: '是否单身'
+          },
+          sex: {
+            type: 'string',
+            title: '性别',
+            enum: ['0', '1'],
+            default: '0'
+          },
+          hobby: {
+            title: '爱好',
+            type: 'array',
+            items: {
+              type: 'string',
+              title: '爱好',
+              enum: ['乒乓球', '足球', '篮球']
             }
           },
-          "friendLinks": {
-            "$id": "#/properties/friendLinks",
-            "type": "array",
-            "title": "友情链接",
-            "items": {
-              "$id": "#/properties/friendLinks/items",
-              "type": "object",
-              "title": "",
-              "required": [
-                "url",
-                "title"
-              ],
-              "properties": {
-                "url": {
-                  "$id": "#/properties/friendLinks/items/properties/url",
-                  "type": "string",
-                  "title": "链接"
-                },
-                "title": {
-                  "$id": "#/properties/friendLinks/items/properties/title",
-                  "type": "string",
-                  "title": "标题"
-                }
-              }
-            }
+          other: {
+            'title': '其他',
+            'type': 'string',
+            'maxlength': 100
           },
-          "qrcodes": {
-            "$id": "#/properties/qrcodes",
-            "type": "array",
-            "title": "二维码",
-            "maxItems": 2,
-            "items": {
-              "$id": "#/properties/qrcodes/items",
-              "type": "object",
-              "title": "",
-              "required": [
-                "image",
-                "title"
-              ],
-              "properties": {
-                "image": {
-                  "$id": "#/properties/qrcodes/items/properties/image",
-                  "type": "string",
-                  "title": "二维码图片",
-                  "format": "image"
-                },
-                "title": {
-                  "$id": "#/properties/qrcodes/items/properties/title",
-                  "type": "string",
-                  "title": "标题"
-                }
-              }
-            }
+          birthday: {
+            title: '出生日期',
+            type: 'string',
+            format: 'date-time'
           },
-          "copyright": {
-            "$id": "#/properties/copyright",
-            "type": "string",
-            "title": "版权信息"
-          },
-          "icp": {
-            "$id": "#/properties/icp",
-            "type": "object",
-            "title": "ICP备案",
-            "required": [
-              "text",
-              "href"
-            ],
-            "properties": {
-              "text": {
-                "$id": "#/properties/icp/text",
-                "type": "string",
-                "title": "备案名"
-              },
-              "href": {
-                "$id": "#/properties/icp/href",
-                "type": "string",
-                "title": "备案链接"
-              }
+          group: {
+            title: '分组',
+            type: 'array',
+            items: {
+              type: 'string',
+              title: '爱好'
             }
           }
-        }
+        },
+        'required': ['name', 'age', 'single', 'sex', 'hobby', 'birthday']
       },
       model: {
-        navs: [
-          {
-            title: '旅行须知',
-            list: [
-              {
-                title: '旅客须知',
-                url: '/help.html?id=5d09ec31cddc4136748d92fe',
-                target: '_self'
-              },
-              {
-                title: '行李须知',
-                url: 'http://www.luckyair.net/help.html?id=5d770d667cb9705325e1eb1c',
-                target: '_self'
-              },
-              {
-                title: '出行准备',
-                url: 'http://www.luckyair.net/help.html?id=5d1b23151771a46b9410ec7a',
-                target: '_self'
-              },
-              {
-                title: '特殊关怀',
-                url: 'http://www.luckyair.net/help.html?id=5d77401791c6dd531559d9bf',
-                target: '_self'
-              },
-              {
-                title: '隐私条款',
-                url: 'http://www.luckyair.net/help.html?id=5d1b236817ca9f6ba06bd3d7',
-                target: '_self'
-              },
-              {
-                title: 'cookie政策',
-                url: 'http://www.luckyair.net/help.html?id=5d1b238b26d6d96b7d5e4a9d',
-                target: '_self'
-              }
-            ]
-          },
-          {
-            title: '关于我们',
-            list: [
-              {
-                title: '公司简介',
-                url: '/aboutus.html?id=5d1b06fc26d6d96b7d5e4a91',
-                target: '_self'
-              },
-              {
-                title: '办公地址',
-                url: '/aboutus.html?id=5d1b0c5bc7c1236b8948bf81',
-                target: '_self'
-              },
-              {
-                title: '航线网络',
-                url: '/aboutus.html?id=5d1b0c81ce9f066b834fd9e6',
-                target: '_self'
-              },
-              {
-                title: '招聘信息',
-                url: 'http://www.luckyair.net/news/5d7762d468a1fc531a66aac4.html',
-                target: '_self'
-              },
-              {
-                title: '满意度调查问卷',
-                url: 'https://luckyaircrm.wjx.com/jq/34852713.aspx',
-                target: '_self'
-              }
-            ]
-          },
-          {
-            title: '联系我们',
-            list: [
-              {
-                title: '官方服务热线',
-                url: '/contactus.html?id=5d09f8e39351fd368bdc2e67',
-                target: '_self'
-              },
-              {
-                title: '投诉建议',
-                url: '/contactus.html?id=5d1b13ea8f41f66b9aa8f4c5',
-                target: '_self'
-              },
-              {
-                title: '媒体专线',
-                url: '/contactus.html?id=5d1b1b4726d6d96b7d5e4a97',
-                target: '_self'
-              },
-              {
-                title: '营业柜台',
-                url: '/contactus.html?id=5d1b1b7b8f41f66b9aa8f4c7',
-                target: '_self'
-              },
-              {
-                title: '货运业务',
-                url: '/contactus.html?id=5d1b1bd4618d536b8e5ddbc6',
-                target: '_self'
-              },
-              {
-                title: '航机媒体广告',
-                url: '/contactus.html?id=5d1b1c34c7c1236b8948bf83',
-                target: '_self'
-              }
-            ]
-          }
-        ],
-        friendLinks: [
-          {
-            url: 'http://www.hnair.com',
-            title: '海南航空'
-          },
-          {
-            url: 'http://www.westair.cn',
-            title: '西部航空'
-          },
-          {
-            url: 'http://www.tianjin-air.com',
-            title: '天津航空'
-          },
-          {
-            url: 'http://www.jdair.net',
-            title: '首都航空'
-          },
-          {
-            url: 'http://www.fuzhou-air.cn',
-            title: '福州航空'
-          },
-          {
-            url: 'http://ffp.hnair.com/FFPClub/cn/hqjf/hklj/gszl/jpcy/1155.shtml',
-            title: '金鹏俱乐部'
-          }
-        ],
-        copyright: '©2012-2020 luckyair.net 云南祥鹏航空有限责任公司 版权所有 技术支持：易航科技股份有限公司',
-        icp: {
-          text: '滇ICP备11002836号',
-          href: 'http://beian.miit.gov.cn/'
-        }
+        name: '王昌彬',
+        age: 20,
+        single: false,
+        sex: '0',
+        hobby: ['足球'],
+        other: '111',
+        birthday: '2020-07-30 12:00:00'
       }
     }
   }
--- examples/src/pages/form/index.js
+++ examples/src/pages/form/index.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import App from './App'
 import ZeroUI from '@/packages'
-import { Form } from 'ant-design-vue'
+import { Form, Slider } from 'ant-design-vue'
 // import '@/packages/Form/style'
 
 Vue.config.productionTip = false
 
 Vue.use(ZeroUI)
 Vue.use(Form)
+Vue.use(Slider)
 
 /* eslint-disable no-new */
 new Vue({
