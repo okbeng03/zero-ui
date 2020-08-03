6f00421690f3c362800ed1146c2944c06a69fe7d
chb.wang <chb.wang@hnair.com>
2020-08-03 15:30:38 +0800

    remove TODO comment

--- packages/Table/core/action.js
+++ packages/Table/core/action.js
@@ -3,8 +3,6 @@ import { isFunction } from 'lodash'
 export default function (actions) {
   const actionsFun = {}
 
-  // TODO: actions 本身就是组件定义的方法，如何处理。应该直接调用。如何判断
-
   for (const key in actions) {
     const action = actions[key]
 
