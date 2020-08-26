f14cb6eaf5c7bc2bfe164697c5e258ac0d2f2c6d
chb.wang <chb.wang@hnair.com>
2020-08-26 16:47:00 +0800

    key串出现开头一致校验问题

--- packages/Form/core/definition/index.js
+++ packages/Form/core/definition/index.js
@@ -89,7 +89,7 @@ function construct (schemaPathMapKeys, parent, parentPath = '') {
 
   leafs.forEach(key => {
     // 找出子节点
-    const childs = remove(keys, item => item.indexOf(key) === 0).map(item => item.replace(key === '[0]' ? '[0].' : new RegExp(`${key}\\.?`), ''))
+    const childs = remove(keys, item => new RegExp(`${key}(\\.|\\[0\\])`).test(item)).map(item => item.replace(key === '[0]' ? '[0].' : new RegExp(`${key}\\.?`), ''))
     const path = `${parentPath}${key}`
 
     if (childs.length) {
