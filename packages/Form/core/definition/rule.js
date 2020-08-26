6d1126e99a57689eff186d9bcad7f2b3dcee9183
chb.wang <chb.wang@hnair.com>
2020-08-26 10:20:57 +0800

    remove rules

--- packages/Form/core/definition/rule.js
+++ packages/Form/core/definition/rule.js
@@ -1,28 +1,28 @@
-import checkboxParse from './rules/checkbox'
-import htmlParse from './rules/html'
-import radioParse from './rules/radio'
-import textareaParse from './rules/textarea'
-import selectParse from '../schema/rules/select'
+// import checkboxParse from './rules/checkbox'
+// import htmlParse from './rules/html'
+// import radioParse from './rules/radio'
+// import textareaParse from './rules/textarea'
+// import selectParse from '../schema/rules/select'
 
 export default [
-  {
-    name: 'checkbox',
-    parse: checkboxParse
-  },
-  {
-    name: 'html',
-    parse: htmlParse
-  },
-  {
-    name: 'radio',
-    parse: radioParse
-  },
-  {
-    name: 'textarea',
-    parse: textareaParse
-  },
-  {
-    name: 'select',
-    parse: selectParse
-  }
+  // {
+  //   name: 'checkbox',
+  //   parse: checkboxParse
+  // },
+  // {
+  //   name: 'html',
+  //   parse: htmlParse
+  // },
+  // {
+  //   name: 'radio',
+  //   parse: radioParse
+  // },
+  // {
+  //   name: 'textarea',
+  //   parse: textareaParse
+  // },
+  // {
+  //   name: 'select',
+  //   parse: selectParse
+  // }
 ]
