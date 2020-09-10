3c40dbf5e085f961c5b33c3c1701cd1925215aac
chb.wang <chb.wang@hnair.com>
2020-09-10 15:22:42 +0800

    table属性改写问题修复

--- packages/Table/core/index.js
+++ packages/Table/core/index.js
@@ -1,8 +1,11 @@
+import extend from 'extend'
 import schemaPath from '../../util/schemaPath'
 import actionParse from './action'
 import addons from './addons'
 
 function parse (definition, schema) {
+  schema = extend(true, {}, schema)
+  definition = extend(true, {}, definition)
   const schemaPathMap = schemaPath(schema)
   const { actions } = definition
   const dsl = {}
